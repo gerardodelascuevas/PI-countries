23ac8ab6593d654f547c8879fa8fd693b59509a9
gerardodelascuevas <gerdlcp@gmail.com>
2022-02-14 08:52:20 -0600

    back routes

--- PI-Countries-main/api/src/models/Activities.js
+++ PI-Countries-main/api/src/models/Activities.js
@@ -3,14 +3,10 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Country', {
+  sequelize.define('Activities', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    flag: {
-      type: DataTypes.STRING, 
-      allowNull: false, 
-    }
   });
 };
--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require('axios')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,54 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+const allCountries = async ()=> {
+ let countryData = await axios.get(`https://restcountries.com/v3/all`)
+ //country data es objeto 
+ countryData = Object.values(countryData).splice(5)[0]
+ //AHORA COUNTRY DATA ES ARRAY
+//  console.log((countryData[0]))
+
+    let result = countryData.map(x=> {
+      return {
+          id: x.fifa,
+          name: x.name.official,
+          capital: x.capital ? x.capital[0] : "We don't have a capital",
+          continent: x.region,
+          languages: x.languages,
+          superficie: x.area,
+          flag: x.flags[0],
+         population: x.population,
+          
+      }
+
+    })
+    //console.log(result)
+    return result
+}
+
+router.get('/country', async (req, res)=> {
+    const { country }  = req.query
+    const apiData =  await allCountries()
+        if(country){
+            const myCountry = apiData.find(x=> x.name.toLowerCase().includes(country.toLocaleLowerCase()) )
+            myCountry.length ? res.send(myCountry) : res.sendStatus(404).send("Sorry your contry doesn't exist")
+        } else res.send(apiData)    
+})
+
+router.get('/country/:id', async(req, res)=> {
+    let { id } = req.params
+    const countries = await allCountries()
+        if(id) {
+        const myCountry = countries.find(x => x.id.toLowerCase() === id.toLowerCase())
+        myCountry ? res.send(myCountry) : res.sendStatus(404).send("Sorry we dont have data")  
+        } else res.send(allCountries)
+})
+
+router.post('/activity', (res, req)=> {
+   
+})
+
 
 
 module.exports = router;
