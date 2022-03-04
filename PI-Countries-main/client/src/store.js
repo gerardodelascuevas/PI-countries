9f7226cd914ed47cddb7e503f76c69727a1c01f4
gerardodelascuevas <gerdlcp@gmail.com>
2022-03-04 12:09:59 -0600

    updating deploy

--- PI-Countries-main/client/src/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware, compose  } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducer'
-
-export const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
